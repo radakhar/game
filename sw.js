2318a97ab27e4b9d174e837c386fa43bb2b77ed3
radakhar <radeakhar@gmail.com>
2026-05-31 18:08:43 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,49 @@
-const CACHE_NAME = 'radakhar-v1';
+const CACHE_NAME = 'radakhar-game-v1';
+
 const urlsToCache = [
   '/game/',
   '/game/index.html',
   '/game/style.css',
-  '/game/script.js'
+  '/game/script.js',
+  '/game/manifest.json',
+  '/game/icons/Icon-اصلی.png',
+  '/game/icons/Icon478.png',
+  '/game/icons/Icon974.png',
+  '/game/screenshots/Screenshot12.png',
+  '/game/screenshots/Screenshot23.png',
+  '/game/screenshots/Screenshot86.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });

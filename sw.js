d8413ce7aa2b2bbd1f939d9aa9102cbd619919b8
radakhar <radeakhar@gmail.com>
2026-05-31 16:47:29 +0330

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'radakhar-v1';
+const urlsToCache = [
+  '/game/',
+  '/game/index.html',
+  '/game/style.css',
+  '/game/script.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
